1307c092e8a63ab16b86096d85c29569e4cb729d
Federico Menendez <federicomenendez@MacBook-Air-de-Federico.local>
2018-05-09 13:15:28 -0300

    Cambios en Search Fragment

--- app/src/main/java/utn/proy2k18/vantrack/SearchFragment.java
+++ app/src/main/java/utn/proy2k18/vantrack/SearchFragment.java
@@ -1,12 +1,23 @@
 package utn.proy2k18.vantrack;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.time.Month;
+import java.util.Calendar;
 
 
 /**
@@ -26,6 +37,12 @@ public class SearchFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    View vista;
+    Button search_date_button;
+    Button search_hour_button;
+    EditText reservation_date;
+    EditText reservation_hour;
+    int dia,mes,anio,hora,minutos;
 
 
 
@@ -68,7 +85,35 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        vista=inflater.inflate(R.layout.fragment_search, container, false);
+
+        search_date_button=(Button)vista.findViewById(R.id.search_date_button);
+        reservation_date=(EditText) vista.findViewById(R.id.reservation_date);
+        search_hour_button=(Button)vista.findViewById(R.id.search_hour_button);
+        reservation_hour=(EditText)vista.findViewById(R.id.reservation_hour);
+
+
+
+
+        search_date_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToCalendarDate(); }
+
+
+        });
+
+
+        search_hour_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToCalendarDay(); }
+
+
+        });
+
+        return vista;
+
 
     }
 
@@ -110,4 +155,51 @@ public class SearchFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
+    public void goToCalendarDate(){
+
+        final Calendar c = Calendar.getInstance();
+        dia = c.get(Calendar.DAY_OF_MONTH);
+        mes = c.get(Calendar.MONTH);
+        anio = c.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                reservation_date.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + (year));
+
+
+            }
+        }, dia, mes, anio);
+        datePickerDialog.updateDate(anio, mes, dia);
+
+        datePickerDialog.show();
+
+    }
+
+    public void goToCalendarDay(){
+
+        final Calendar c = Calendar.getInstance();
+        hora = c.get(Calendar.HOUR_OF_DAY);
+        minutos = c.get(Calendar.MINUTE);
+
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                reservation_hour.setText(hourOfDay+":"+minute);
+
+            }
+        },hora,minutos,false);
+
+        timePickerDialog.updateTime(hora,minutos);
+        timePickerDialog.show();
+
+    }
+
+
+
 }
