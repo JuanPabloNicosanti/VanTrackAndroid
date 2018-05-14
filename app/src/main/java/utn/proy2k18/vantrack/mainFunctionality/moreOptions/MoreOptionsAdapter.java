031d0ce00ff29ee5f311743b4f501155d7c259ba
JuanNicosanti <juan.nicosanti@mercadolibre.com>
2018-05-14 01:17:14 -0300

    Adds Option and MoreOptionsAdapter classes

--- app/src/main/java/utn/proy2k18/vantrack/mainFunctionality/moreOptions/MoreOptionsAdapter.java
+++ app/src/main/java/utn/proy2k18/vantrack/mainFunctionality/moreOptions/MoreOptionsAdapter.java
@@ -1,6 +1,39 @@
 package utn.proy2k18.vantrack.mainFunctionality.moreOptions;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MoreOptionsAdapter extends ArrayAdapter {
+import java.util.ArrayList;
+
+import utn.proy2k18.vantrack.R;
+
+public class MoreOptionsAdapter extends ArrayAdapter<Option> {
+
+    public MoreOptionsAdapter(Context context, ArrayList<Option> objects) {
+        super(context, 0, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Option option = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.option, parent, false);
+        }
+        // Lookup view for data population
+        TextView opName = (TextView) convertView.findViewById(R.id.option_title);
+        ImageView opImage = (ImageView) convertView.findViewById(R.id.option_image);
+        // Populate the data into the template view using the data object
+        opName.setText(option.getTitle());
+        opImage.setImageResource(option.getImage());
+        // Return the completed view to render on screen
+        return convertView;
+    }
+
 }
--- app/src/main/java/utn/proy2k18/vantrack/mainFunctionality/moreOptions/Option.java
+++ app/src/main/java/utn/proy2k18/vantrack/mainFunctionality/moreOptions/Option.java
@@ -1,4 +1,20 @@
 package utn.proy2k18.vantrack.mainFunctionality.moreOptions;
 
 public class Option {
+
+    private String title;
+    private int image;
+
+    public Option(String title, int image) {
+        this.title = title;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getImage() {
+        return image;
+    }
 }
