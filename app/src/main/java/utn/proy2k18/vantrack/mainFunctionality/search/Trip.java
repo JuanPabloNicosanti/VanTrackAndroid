5e472b1418a1cd31568cc43e1f0a638424776607
JuanPabloNicosanti <jpnicosanti@gmail.com>
2018-06-03 21:02:37 -0300

    Adds Trip and TripAdapter classes

---
+++ app/src/main/java/utn/proy2k18/vantrack/mainFunctionality/search/Trip.java
@@ -0,0 +1,41 @@
+package utn.proy2k18.vantrack.mainFunctionality.search;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Trip {
+
+    private String company;
+    private Date date;
+    private String origin;
+    private String destination;
+
+    public Trip(String company, Date date, String origin, String destination) {
+        this.company = company;
+        this.date = date;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getFormattedDate(){
+        SimpleDateFormat ft = new SimpleDateFormat("E MM-dd hh:mm a");
+        return ft.format(date);
+    }
+
+}
---
+++ app/src/main/java/utn/proy2k18/vantrack/mainFunctionality/search/TripsAdapter.java
@@ -0,0 +1,65 @@
+package utn.proy2k18.vantrack.mainFunctionality.search;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import utn.proy2k18.vantrack.R;
+
+public class TripsAdapter extends RecyclerView.Adapter<TripsAdapter.ModelViewHolder> {
+
+    private List<Trip> items;
+
+    public TripsAdapter(List<Trip> items) {
+        this.items = items;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public TripsAdapter.ModelViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ModelViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.trip, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(TripsAdapter.ModelViewHolder holder, int position) {
+        holder.bind(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items != null ? items.size() : 0;
+    }
+
+    public class ModelViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView company;
+        private TextView date;
+        private TextView origin;
+        private TextView destination;
+        //TODO: Poner todos los atributos de la reserva para bindearlos
+
+        public ModelViewHolder(View itemView) {
+            super(itemView);
+            this.company = itemView.findViewById(R.id.company);
+            this.date = itemView.findViewById(R.id.date);
+            this.origin = itemView.findViewById(R.id.origin);
+            this.destination = itemView.findViewById(R.id.destination);
+        }
+
+        public void bind(Trip trip) {
+            company.setText(trip.getCompany());
+            date.setText(trip.getFormattedDate());
+            origin.setText(trip.getOrigin());
+            destination.setText(trip.getDestination());
+        }
+    }
+
+}
