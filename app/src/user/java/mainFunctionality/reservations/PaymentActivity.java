0a1c0f0329a92b876aa766c369894745a90fe9af
JuanPabloNicosanti <jpnicosanti@gmail.com>
2018-09-09 22:12:19 -0300

    Update MP access token

--- app/src/user/java/mainFunctionality/reservations/PaymentActivity.java
+++ app/src/user/java/mainFunctionality/reservations/PaymentActivity.java
@@ -26,7 +26,7 @@ import utn.proy2k18.vantrack.utils.QueryBuilder;
 
 public class PaymentActivity extends AppCompatActivity {
     private String PUBLIC_KEY="TEST-661496e3-25fc-46c5-a4c8-4d05f64f5936";
-//    private String ACCESS_TOKEN="TEST-7052649783174585-082122-45bff91292a5ff2a966379591121df97-172493186";
+//    private String ACCESS_TOKEN="TEST-5222723668192320-090920-796f2538a130ff517ec2e1740e5d3e4d-353030546";
     private String reservationId;
     private float reservationPrice;
     private QueryBuilder queryBuilder = new QueryBuilder();
