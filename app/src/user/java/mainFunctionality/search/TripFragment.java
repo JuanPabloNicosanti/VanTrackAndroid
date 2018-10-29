5a8a2f0928d76e7be83948bc2a5400bdac040c56
JuanPabloNicosanti <jpnicosanti@gmail.com>
2018-10-29 00:45:01 -0300

    More hotfixes

--- app/src/user/java/mainFunctionality/search/TripFragment.java
+++ app/src/user/java/mainFunctionality/search/TripFragment.java
@@ -3,6 +3,7 @@ package mainFunctionality.search;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
