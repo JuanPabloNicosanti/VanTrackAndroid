79619640ba292a8f3e57360f803e942df6f7f8d0
JuanPabloNicosanti <jpnicosanti@gmail.com>
2018-11-25 01:56:40 -0300

    Updates extra charge popup dialog message

--- app/src/driver/java/mainFunctionality/driverTrips/TripFragment.java
+++ app/src/driver/java/mainFunctionality/driverTrips/TripFragment.java
@@ -314,7 +314,6 @@ public class TripFragment extends Fragment {
     private void setFragment(Fragment fragment) {
         FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment_container, fragment);
-        ft.addToBackStack(null);
         ft.commit();
     }
 
